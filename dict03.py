9eb5ae7ab68ed78a0a926c030132966ffce31420
Aakash Chaudhry <vcr3at3@gmail.com>
2020-04-23 11:17:11 +0530

    Delete dict03.py

--- dict03.py
+++
@@ -1,7 +0,0 @@
-employees = ['Kelly', 'Emma', 'John']
-defaults = {"designation": 'Application Developer', "salary": 8000}
-
-resDict = dict.fromkeys(employees, defaults)
-print(resDict)
-
-print(resDict["Kelly"])
